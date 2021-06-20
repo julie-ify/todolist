3278e585b4bf40a73273fa6a0e3f0552182b12ff
Juliana Ifionu <julieifionu@gmail.com>
2021-06-20 17:05:11 +0100

    add or remove items from list

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,13 +1,14 @@
-
-// Get  Element by DOM 
+// Get  Element by DOM
 const searchBtn = document.getElementById("search");
 const listItems = document.getElementById("list-items");
 const listItem = document.querySelectorAll(".list-item");
 const addList = document.getElementById("input-add");
-const form = document.getElementById('input-wrap');
+const form = document.getElementById("input-wrap");
+// const deleteBtn = document.querySelectorAll(".btn");
 
-// Add eventlistener to form on submission
+// Add eventlistener to form
 form.addEventListener("submit", searchItem);
+
 function searchItem(e) {
   //prevent default form submission by html
   e.preventDefault();
@@ -19,7 +20,7 @@ function searchItem(e) {
   //create button element and append text content
   let listBtn = document.createElement("button");
   listBtn.className = "btn";
-  listBtn.append('x');
+  listBtn.append("x");
 
   //create li element and append h4 and button
   let newList = document.createElement("li");
@@ -29,3 +30,44 @@ function searchItem(e) {
   //append newlist to the ul
   listItems.append(newList);
 }
+
+//Delete item function
+  
+  // for (let i = 0; i < deleteBtn.length; i++) {
+  //   deleteBtn[i].addEventListener("click", deleteItem);
+  // }
+  // function deleteItem() {
+  //   if(confirm('Are you sure?')){
+  //     let badItem = this.parentElement;
+  //     listItems.removeChild(badItem);
+      
+  //   }
+  // }
+
+// deleteBtn.forEach((item) => {
+//   item.addEventListener('click', deleteItem);
+//   function deleteItem() {
+//     if(this.classList.contains('btn')){
+//       if(confirm('Are you sure?')){
+//         let badItem = this.parentElement;
+//         listItems.removeChild(badItem);
+        
+//       }
+//     }
+//     }
+// })
+
+listItems.addEventListener('click', deleteItem);
+
+function deleteItem(e) {
+  if(e.target.classList.contains('btn')){
+    if(confirm('Are you sure?')){
+      let badItem = e.target.parentElement;
+      listItems.removeChild(badItem);
+      
+    }
+  }
+  }
+
+  
+
